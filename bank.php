de0d0b88e2bcfdce91fa2e8f1862d12068caa580
broker250 <obisalemhahirwa@gmail.com>
2025-01-10 12:26:15 +0200

    Add files via upload

---
+++ bank.php
@@ -0,0 +1,8 @@
+ if user['balance'] >= amount:
+        conn.execute('UPDATE users SET balance = balance + ? WHERE account_number = ?', 
+                     (amount, target_account_number))
+        conn.commit()
+
+        conn.execute('UPDATE users SET balance = balance - ? WHERE account_number = ?', 
+                     (amount, session['user']))
+        conn.commit()
